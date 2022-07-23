047d3875bd65f2a4a28e809435933e495db2fdcd
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-07-23 14:55:05 -0400

    changed sizing of footer

--- react-port/src/components/Footer/Footer.js
+++ react-port/src/components/Footer/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
             <div className= "footer">
                 <a href="https://github.com/KaylaVangel">
                     <img alt="GitHub" src="Images/GitHubicon.PNG"
-                        width="70" height="70"/>
+                        width="50" height="50"/>
                 </a>
                 <a href="https://www.linkedin.com/in/kayla-vangel-346a95230/">
                     <img alt="LinkedIn" src="Images/LinkedIn icon.PNG"
-                        width="70" height="70"/>
+                        width="50" height="50"/>
                 </a>
-                <p>Created by Kayla</p>
+                
 
             </div>
         </>
