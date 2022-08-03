84266f80724d8ee60bbc23069a482a3ff21c0483
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-08-03 19:26:57 -0400

    changed resume

--- src/components/Nav/Resume.js
+++ src/components/Nav/Resume.js
@@ -10,12 +10,12 @@ const Resume = () => {
                     <li>JavaScript</li>
                     <li>Node.js</li>
                     <li>Express.js</li>
-                    <li>SQL</li>
-                    <li>MySQL</li>
                     <li>React</li>
                     <li>MongoDB</li>
-                    <li>Mongoose</li>
-                    <li>JavaScript</li>
+                    <li>GraphQl</li>
+                    <li>SQL</li>
+                    <li>Bootstrap</li>
+                    <li>JMUI</li>
                 </ul>
             </p>
             <a href="./resume.docx">Downloadable Resume</a>
